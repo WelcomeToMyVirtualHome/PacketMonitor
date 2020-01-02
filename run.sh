a2751302c44ff91f1fabe09fe9e758f63f094ec3
Jakub Sobolewski <jakupsob@gmail.com>
2020-01-02 20:06:58 +0100

    Run.sh update

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ docker run -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -it -h e
 docker run -d -p 5601:5601 -h kibana --name kibana --link elasticsearch:elasticsearch docker.elastic.co/kibana/kibana:7.5.1
 # docker run -h logstash --name logstash --link elasticsearch:elasticsearch -it --rm -v "$PWD"/logstash_config:/usr/share/logstash/config docker.elastic.co/logstash/logstash:7.5.1 -f /usr/share/logstash/config/logstash.conf
 # docker run --name filebeat -v "$PWD"/filebeat.yml:/usr/share/filebeat/filebeat.yml docker.elastic.co/beats/filebeat:7.5.1
-sudo filebeat -e -c filebeat_config/filebeat.yml
+sudo filebeat -e -c "$PWD"/filebeat_config/filebeat.yml
